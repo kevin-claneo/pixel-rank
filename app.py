23e794013dfb3e8487f6a3ae504371e1d7f210f4
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-01-10 13:49:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,37 @@ def setup_streamlit():
     st.title("SERP Analysis with DataForSEO")
     st.markdown("## Analyze the SERP pixel ranks for a set of keywords")
 
+# creating the Restclient
+class RestClient:
+    domain = "api.dataforseo.com"
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def request(self, path, method, data=None):
+        connection = HTTPSConnection(self.domain)
+        try:
+            base64_bytes = b64encode(
+                ("%s:%s" % (self.username, self.password)).encode("ascii")
+                ).decode("ascii")
+            headers = {'Authorization' : 'Basic %s' %  base64_bytes, 'Content-Encoding' : 'gzip'}
+            connection.request(method, path, headers=headers, body=data)
+            response = connection.getresponse()
+            return loads(response.read().decode())
+        finally:
+            connection.close()
+
+    def get(self, path):
+        return self.request(path, 'GET')
+
+    def post(self, path, data):
+        if isinstance(data, str):
+            data_str = data
+        else:
+            data_str = dumps(data)
+        return self.request(path, 'POST', data_str)
+
 # Function to authenticate and fetch pixel ranks from DataForSEO
 def get_pixel_ranks(keywords, language, country, device, username, password):
     base_url = "https://api.dataforseo.com/"
