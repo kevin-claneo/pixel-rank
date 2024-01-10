97d44df36b917de98089d74cb28bf2eed16cd91a
kevin-claneo <108477383+kevin-claneo@users.noreply.github.com>
2024-01-10 14:01:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import json
 
 # Streamlit App Configuration
 def setup_streamlit():
-    st.set_page_config(page_title="PixelRank with DataForSEO", page_icon=":framed_picture:", layout="wide")
-    st.title(":framed_picture: :medal_sports: PixelRank with DataForSEO")
+    st.set_page_config(page_title="PixelRank with DataForSEO", page_icon="🏅", layout="wide")
+    st.title("🏅 PixelRank with DataForSEO")
     st.markdown("## Analyze the SERP pixel ranks for a set of keywords")
 
 # creating the Restclient
